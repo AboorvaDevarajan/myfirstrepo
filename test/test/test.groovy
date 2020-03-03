c31713d873d59e9ddfda51de09baac45635f5bda
Aboorva Devarajan <aburvadevarajan@gmail.com>
2020-03-03 17:49:57 +0530

    Update test.groovy

--- test/test/test.groovy
+++ test/test/test.groovy
@@ -1,7 +1,6 @@
 import groovy.transform.Field
-
-@Field def var1 = "test"
-@Field def var2 = var1 + "test"
+def var1 = "test"
+def var2 = var1 + "test"
 node("master") {
   stage("test"){
     println(var2)
